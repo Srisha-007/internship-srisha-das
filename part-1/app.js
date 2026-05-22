bcea987cf685a3d3badacc5a46f6321b724a5220
Srisha-007 <srishadas24@gmail.com>
2026-05-22 20:15:43 +0530

    feat: Added dynamic hero banner

--- part-1/app.js
+++ part-1/app.js
@@ -2,55 +2,91 @@ const BASE_URL="https://api.themoviedb.org/3";
 const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500";
 
 const moviesGrid = document.getElementById("moviesGrid");
-
+// =====================================
 // Fetch Popular Movies from TMDB API
+// =====================================
 async function fetchPopularMovies() {       
     try{
-        const response = await fetch(`${BASE_URL}/movie/popular?api_key=${TMDB_API_KEY}`);
+        const response = await fetch(
+            `${BASE_URL}/movie/popular?api_key=${TMDB_API_KEY}`
+        );
+
         if (!response.ok) {
             throw new Error(`HTTP Error: ${response.status}`);
         }
         const data = await response.json();
-        console.log(data.results);
+        renderMovies(data.results);
+        setHeroBanner(data.results[0]);
     }
     catch (error){
         console.error("Error fetching movies:", error);
     }
 }
-// Render movies in the grid
+
+// =====================================
+//           Render Movies
+// =====================================
 function renderMovies(movies) {
-    moviesGrid.innerHTML = ""; // Clear existing movies
-    if (movies.length==0){
-        moviesGrid.innerHTML = `
-        <p class="empty-state">No movies found.</p>`;
-        return;
-    }
+    moviesGrid.innerHTML = "";
+
     movies.forEach((movie) => {
         const movieCard = document.createElement("article");
         movieCard.classList.add("movie-card");
-        movieCard.dataset.movieId = movie.id;
+        moviesGrid.innerHTML = `
+            <div class="card-poster-container">
+                <img
+                    class="card-poster"
+                    src="${IMAGE_BASE_URL + movie.poster_path}"
+                    alt="${movie.title}"
+                >
+
+                <div class="card-rating-badge">
+                    ${movie.vote_average.toFixed(1)}
+                </div>
+            </div>
+
+            <div class="card-info">
+                <h3 class="card-title">
+                    ${movie.title}
+                </h3>
+
+                <div class="card-metadata">
+                    <span>
+                        ${movie.release_date?.split("-")[0] || "N/A"}
+                    </span>
+                    <span>
+                        Popular
+                    </span>
+                </div>
+            </div>
+            `;
+            moviesGrid.appendChild(movieCard);
     });
 }
-
-// Hero Banner
+// =====================================
+//           Set Hero Banner 
+// =====================================
 function setHeroBanner(movie) {
     const heroTitle = document.getElementById("heroTitle");
     const heroMeta = document.getElementById("heroMeta");
     const heroDescription = document.getElementById("heroDescription");
 
     heroTitle.textContent = movie.title;
-    heroMeta.textContent = `${new Date(movie.release_date).getFullYear()} | ${movie.genre_ids.join(", ")} | ${movie.vote_average}`;
+
+    heroMeta.textContent =
+        `${movie.release_date} • ${movie.vote_average.toFixed(1)}`;
+
     heroDescription.textContent = movie.overview;
 
     document.querySelector(".hero-section").style.backgroundImage = `
+
         linear-gradient(
             to right,
-            rgba(15, 23, 42, 0.95) 40%,
-            rgba(15, 23, 42, 0.6)
+            rgba(15,23,42,0.95) 40%,
+            rgba(15,23,42,0.6)
         ),
         url(https://image.tmdb.org/t/p/original${movie.backdrop_path})
     `;
 }
-
 // Initial fetch of popular movies when the page loads          
 fetchPopularMovies();
