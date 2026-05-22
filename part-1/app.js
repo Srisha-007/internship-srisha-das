7ece9ac88c07b9e2424d25f4e8f9b723593105c1
Srisha-007 <srishadas24@gmail.com>
2026-05-22 20:02:25 +0530

    feat: Render movie cards dynamically from TMDB data

--- part-1/app.js
+++ part-1/app.js
@@ -6,7 +6,7 @@ const moviesGrid = document.getElementById("moviesGrid");
 // Fetch Popular Movies from TMDB API
 async function fetchPopularMovies() {       
     try{
-        const response = await fetch(`${BASE_URL}/movie/popular?api_key=${CONFIG.TMDB_API_KEY}`);
+        const response = await fetch(`${BASE_URL}/movie/popular?api_key=${TMDB_API_KEY}`);
         if (!response.ok) {
             throw new Error(`HTTP Error: ${response.status}`);
         }
@@ -16,6 +16,40 @@ async function fetchPopularMovies() {
     catch (error){
         console.error("Error fetching movies:", error);
     }
+}
+// Render movies in the grid
+function renderMovies(movies) {
+    moviesGrid.innerHTML = ""; // Clear existing movies
+    if (movies.length==0){
+        moviesGrid.innerHTML = `
+        <p class="empty-state">No movies found.</p>`;
+        return;
+    }
+    movies.forEach((movie) => {
+        const movieCard = document.createElement("article");
+        movieCard.classList.add("movie-card");
+        movieCard.dataset.movieId = movie.id;
+    });
+}
+
+// Hero Banner
+function setHeroBanner(movie) {
+    const heroTitle = document.getElementById("heroTitle");
+    const heroMeta = document.getElementById("heroMeta");
+    const heroDescription = document.getElementById("heroDescription");
+
+    heroTitle.textContent = movie.title;
+    heroMeta.textContent = `${new Date(movie.release_date).getFullYear()} | ${movie.genre_ids.join(", ")} | ${movie.vote_average}`;
+    heroDescription.textContent = movie.overview;
+
+    document.querySelector(".hero-section").style.backgroundImage = `
+        linear-gradient(
+            to right,
+            rgba(15, 23, 42, 0.95) 40%,
+            rgba(15, 23, 42, 0.6)
+        ),
+        url(https://image.tmdb.org/t/p/original${movie.backdrop_path})
+    `;
 }
 
 // Initial fetch of popular movies when the page loads          
