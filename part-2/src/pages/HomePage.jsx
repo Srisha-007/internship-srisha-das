2a2c4413519adbe3aef9ca8d4aa5d6a1458cdc20
Srisha-007 <srishadas24@gmail.com>
2026-06-15 16:18:53 +0530

    feat: Added initial Navbar component

--- part-2/src/pages/HomePage.jsx
+++ part-2/src/pages/HomePage.jsx
@@ -1,9 +1,11 @@
+import Navbar from "../components/Navbar/Navbar";
+
 function HomePage() {
     return (
-        <div>
+        <>
+            <Navbar />
             <h1>Home Page</h1>
-            <p>Welcome to the home page!</p>
-        </div>
+        </>
     );
 }
 
