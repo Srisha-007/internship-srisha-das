6f3de0e38437a0d5766f66f03794b43f8a29cb4b
Srisha-007 <srishadas24@gmail.com>
2026-06-16 11:21:33 +0530

    feat: render raw TMDB movie data for inspection

--- part-2/src/pages/HomePage.jsx
+++ part-2/src/pages/HomePage.jsx
@@ -1,12 +1,14 @@
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import { getPopularMovies } from "../services/tmdb";
 
 function HomePage() {
+    const [movies, setMovies] = useState([]);
+    
     useEffect(() => {
         async function fetchMovies() {
             try {
                 const data = await getPopularMovies();
-                console.log(data);
+                setMovies(data.results);
             }
             catch (error) {
                 console.error(error);
@@ -15,7 +17,16 @@ function HomePage() {
         fetchMovies();
     }, []);
 
-    return <h1>Home Page</h1>;
+    return (
+        <div>
+            <h1>Home Page</h1>
+            {movies.length >0 && (
+                <pre>
+                    {JSON.stringify(movies[0], null, 2)}
+                </pre>
+            )}
+        </div>
+    );
 }
 
 export default HomePage;
