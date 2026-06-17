079240626718aa13521e87da276fb92459fc1bac
Srisha-007 <srishadas24@gmail.com>
2026-06-17 17:11:10 +0400

    refactor: moved movie fetching logic into useMovies hook

--- part-2/src/hooks/useMovies.js
+++ part-2/src/hooks/useMovies.js
@@ -1,1 +1,29 @@
-export {};
+import {useEffect, useState} from "react";
+import { getPopularMovies } from "../services/tmdb";
+
+export function useMovies() {
+    const [movies, setMovies] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState("");
+
+    useEffect(() => {
+        async function loadMovies() {
+            try {
+                const data = await getPopularMovies();
+                setMovies(data.results);
+            }
+            catch (error) {
+                console.error(error);
+                setError(error.message || "Failed to load movies. Please try again later.");
+            }
+            finally {
+                setLoading(false);
+            }
+        }
+        loadMovies();
+    }, []);
+
+    return {
+        movies, loading, error
+    };
+}
--- part-2/src/pages/HomePage.jsx
+++ part-2/src/pages/HomePage.jsx
@@ -1,28 +1,8 @@
-import {useEffect, useState} from "react";
-import { getPopularMovies } from "../services/tmdb";
+import { useMovies } from "../hooks/useMovies";
 import MovieCard from "../components/MovieCard/MovieCard";
 
-function HomePage() {
-    const [movies, setMovies] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState("");
-
-    useEffect(() => {
-        async function loadMovies() {
-            try {
-                const data = await getPopularMovies();
-                setMovies(data.results);
-            }
-            catch (error) {
-                console.error(error);
-                setError(error.message || "Failed to load movies. Please try again later.");
-            }
-            finally {
-                setLoading(false);
-            }
-        }
-        loadMovies();
-    }, []);
+ function HomePage() {
+    const { movies, loading, error } = useMovies();
 
     return (
         <div>
@@ -30,7 +10,7 @@ function HomePage() {
 
             {loading && <p>Loading movies...</p>}
             {error && <p style={{ color: "red" }}>{error}</p>}
-            
+
             {!loading && !error && 
                 movies.map((movie) => (
                     <MovieCard key={movie.id} movie={movie} />
