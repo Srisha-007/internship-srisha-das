5f82e5e541970a380311eabadf535514f4f38ea1
Srisha-007 <srishadas24@gmail.com>
2026-06-18 18:21:21 +0400

    refactor: replace custom logo svg with lucide film icon

--- part-2/src/components/Navbar/Navbar.jsx
+++ part-2/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import styles from "./Navbar.module.css";
-import { Sun } from "lucide-react";
+import { Sun, Film } from "lucide-react";
 
 function Navbar() {
     const activeSection = "trending";
@@ -8,27 +8,7 @@ function Navbar() {
         <header className={styles.mainHeader}>
             <div className={`container ${styles.navbar}`}>
                 <a href="#" className={styles.logoContainer}>
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        strokeWidth="2.5"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        className={styles.logoIcon}
-                    >
-                        <rect width="18" height="18" x="3" y="3" rx="2" />
-                        <path d="M7 3v18" />
-                        <path d="M17 3v18" />
-                        <path d="M3 7h4" />
-                        <path d="M17 7h4" />
-                        <path d="M3 12h18" />
-                        <path d="M3 17h4" />
-                        <path d="M17 17h4" />
-                    </svg>
+                    <Film className={styles.logoIcon} />
 
                     <span>CineScope</span>
                 </a>
