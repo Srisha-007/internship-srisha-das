a544b0931e09e8d2e9a50cb175788100df170cc3
Srisha-007 <srishadas24@gmail.com>
2026-06-18 17:44:55 +0400

    feat: created Navbar component structure

--- part-2/src/components/Navbar/Navbar.jsx
+++ part-2/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,56 @@
+import styles from "./Navbar.module.css";
+import { Sun } from "lucide-react";
+
 function Navbar() {
-    return (
-        <nav>
-            <h2>CineScope</h2>
+  return (
+    <header className={styles.mainHeader}>
+      <div className={`container ${styles.navbar}`}>
+        <a href="#" className={styles.logoContainer}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className={styles.logoIcon}
+          >
+            <rect width="18" height="18" x="3" y="3" rx="2" />
+            <path d="M7 3v18" />
+            <path d="M17 3v18" />
+            <path d="M3 7h4" />
+            <path d="M17 7h4" />
+            <path d="M3 12h18" />
+            <path d="M3 17h4" />
+            <path d="M17 17h4" />
+          </svg>
+
+          <span>CineScope</span>
+        </a>
+
+        <nav className={styles.navLinks}>
+          <a href="#trending" className={`${styles.navLink} ${styles.active}`}>
+            Trending
+          </a>
+
+          <a href="#search" className={styles.navLink}>
+            Search
+          </a>
+
+          <button
+            type="button"
+            className={styles.themeToggle}
+            aria-label="Toggle Theme"
+          >
+            <Sun />
+          </button>
         </nav>
-    );
+      </div>
+    </header>
+  );
 }
 
 export default Navbar;
