c1ec9d7810409e75e327e540a90a54b0b806c789
Srisha-007 <srishadas24@gmail.com>
2026-06-29 15:34:18 +0400

    feat: created hook for fetching movie trailers

---
+++ part-2/src/hooks/useMovieTrailer.js
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import { getMovieVideos } from "../services/tmdb";
+
+export function useMovieTrailer(movieId) {
+    const [trailerKey, setTrailerKey] = useState("");
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState("");
+
+    useEffect(() => {
+        if (!movieId) return;
+
+        async function loadTrailer() {
+            try {
+                setLoading(true);
+                setError("");
+
+                const data = await getMovieVideos(movieId);
+
+                const trailer = data.results.find(
+                    (video) =>
+                        video.site === "YouTube" &&
+                        video.type === "Trailer"
+                );
+
+                setTrailerKey(trailer?.key || "");
+            }
+            catch (error) {
+                setError(error.message || "Failed to load trailer.");
+            }
+            finally {
+                setLoading(false);
+            }
+        }
+
+        loadTrailer();
+    }, [movieId]);
+
+    return {
+        trailerKey, loading, error
+    };
+}
