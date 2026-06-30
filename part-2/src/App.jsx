6ea1cd112fb4debf568b1669c00b863a78a0958d
Srisha-007 <srishadas24@gmail.com>
2026-06-30 12:11:27 +0400

    feat: add global scroll-to-top on route navigation

--- part-2/src/App.jsx
+++ part-2/src/App.jsx
@@ -4,10 +4,13 @@ import { FavoritesProvider } from "./context/FavoritesContext";
 import HomePage from "./pages/HomePage";
 import MovieDetailPage from "./pages/MovieDetailPage";
 import FavoritesPage from "./pages/FavoritesPage";
+import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
 
 function App() {
   return (
     <FavoritesProvider>
+      <ScrollToTop />
+
       <Routes>
         <Route 
           path="/" 
