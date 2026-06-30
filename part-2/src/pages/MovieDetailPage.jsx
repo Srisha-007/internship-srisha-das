e4cbf1eacb9629635475288c19b8adda7fa2e6b6
Srisha-007 <srishadas24@gmail.com>
2026-06-30 13:50:59 +0400

    feat: allow users to submit movie ratings via TMDB API

--- part-2/src/pages/MovieDetailPage.jsx
+++ part-2/src/pages/MovieDetailPage.jsx
@@ -10,6 +10,7 @@ import { useMovieRecommendations } from "../hooks/useMovieRecommendations";
 import MovieCard from "../components/MovieCard/MovieCard";
 import TrailerModal from "../components/TrailerModal/TrailerModal";
 import { useMovieTrailer } from "../hooks/useMovieTrailer";
+import MovieRating from "../components/MovieRating/MovieRating";
 
 import styles from "./MovieDetailPage.module.css";
 
@@ -199,6 +200,8 @@ function MovieDetailPage() {
                     <p className={styles.overview}>
                         {movie.overview}
                     </p>
+
+                    <MovieRating movieId={movie.id} />
 
                     <section className={styles.additionalInfo}>
                         <h3 className={styles.sectionHeading}>
