fb906f95e4330c2098c1b65a060c73253a93b955
Srisha-007 <srishadas24@gmail.com>
2026-06-30 12:30:28 +0400

    feat: persist TMDB guest session for movie ratings

---
+++ part-2/src/hooks/useGuestSession.js
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import { createGuestSession } from "../services/tmdb";
+
+export function useGuestSession() {
+    const [guestSessionId, setGuestSessionId] = useState("");
+
+    useEffect(() => {
+        async function initSession() {
+            const storedSession =
+                localStorage.getItem("guestSessionId");
+
+            if (storedSession) {
+                setGuestSessionId(storedSession);
+                return;
+            }
+
+            try {
+                const data = await createGuestSession();
+
+                localStorage.setItem(
+                    "guestSessionId",
+                    data.guest_session_id
+                );
+
+                setGuestSessionId(
+                    data.guest_session_id
+                );
+            }
+            catch (error) {
+                console.error(error);
+            }
+        }
+
+        initSession();
+    }, []);
+
+    return guestSessionId;
+}
